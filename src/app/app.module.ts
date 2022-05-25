70f23e68a3eb1d8993ff7ea651b227754ba9f9bd
carlos <i201910263@cibertec.edu.pe>
2022-05-24 20:05:17 -0500

    creacion de estructura basica angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { TrabajadorListarComponent } from './trabajador/trabajador-listar/trabajador-listar.component';
+import { TrabajadorCrearComponent } from './trabajador/trabajador-crear/trabajador-crear.component';
+import { VehiculoCrearComponent } from './vehiculo/vehiculo-crear/vehiculo-crear.component';
+import { VehiculoListarComponent } from './vehiculo/vehiculo-listar/vehiculo-listar.component';
+import { LoginComponent } from './auth/login/login.component';
+import { UserCrearComponent } from './auth/user/user-crear/user-crear.component';
+import { UserListarComponent } from './auth/user/user-listar/user-listar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrabajadorListarComponent,
+    TrabajadorCrearComponent,
+    VehiculoCrearComponent,
+    VehiculoListarComponent,
+    LoginComponent,
+    UserCrearComponent,
+    UserListarComponent
   ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/trabajador/trabajador-crear/trabajador-crear.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trabajador-crear',
+  templateUrl: './trabajador-crear.component.html',
+  styleUrls: ['./trabajador-crear.component.css']
+})
+export class TrabajadorCrearComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
