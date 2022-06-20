4d21f9fe1f90fefd8b167866a12f226661bdd746
carlos <i201910263@cibertec.edu.pe>
2022-06-19 19:12:20 -0500

    update proyect

--- src/app/guards/skyb-guards.service.ts
+++ src/app/guards/skyb-guards.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { TokenService } from '../service/token.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class SkybGuardsService {
-
-  constructor() { }
+export class SkybGuardsService implements CanActivate{
+  realRol :string;
+  constructor( private tokenService: TokenService,private router: Router,) { }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    // console.log(route.data)
+  const  expectedRol = route.data['expectedRol'];
+    const roles = this.tokenService.getAuthorities();
+    this.realRol = 'user';
+    roles.forEach(rol => {
+      if(rol === 'ROLE_ADMIN'){
+        this.realRol = 'admin';
+      }
+    });
+    if(!this.tokenService.getToken() || expectedRol.indexOf(this.realRol) === -1){
+      this.router.navigate(['/viaje']);
+      return false;
+    }
+    return true;
+  }
 }
