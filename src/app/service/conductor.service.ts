31c876a611f3a660384e0e124b8643cee3ce7094
Alexis <virgo10sv@gmail.com>
2022-06-24 00:59:35 -0500

    Listado de conductores realizado

--- src/app/service/conductor.service.ts
+++ src/app/service/conductor.service.ts
@@ -1,42 +1,15 @@
-import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Conductor } from "src/app/models/conductor";
-import { environment } from "src/environments/environment";
-import { Observable } from "rxjs";
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
     providedIn: 'root'
 })
 
 export class ConductorService {
-/*
-    constructor(private httpclient: HttpClient){}
 
-    public ConductorSave(conductor: Conductor){
-        return this.httpclient.post<Conductor>("http://localhost:8080/conductor/registrar",conductor);
-    }
-    public ConductorList(estado: string, page: number, size: number, order: string, asc: boolean) {
-
-        let httpParams = new HttpParams(
-          {
-            fromObject: {
-              page: page,
-              estado: estado,
-              order: order,
-              asc: asc
-            }
-          }
-        );
-    
-        return this.httpclient.post(
-          "http://localhost:8080/conductor/listar",
-          httpParams.toString(),
-          {
-            headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
-          }
-        )
-    }*/
-    private apiUrl = `${environment.API_URL}/conductor/`;
+  private apiUrl = `${environment.API_URL}/conductor/`;
     constructor(private HttpClient: HttpClient){ }
 
     public conductorListar( page: number, pageSize: number): Observable<any>{
